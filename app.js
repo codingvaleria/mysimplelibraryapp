f3a938d42d1f9d265c9d399a4b1d2d533e62f5bd
codingvaleria <codingvaleria@gmail.com>
2022-09-13 12:49:09 +0300

    implemented loops

--- app.js
+++ app.js
@@ -25,47 +25,59 @@ const listOfBooks = [
     }
 ]
 
-const menu = ["Show Books", "Add Books", "Lease Book", "Return Book", "Log out"];
-
-let tries = "true"
+const menu = ["Show Books", "Add Books", "View list of Books", "Log out"];
 const welcomeMessage = (`
 > ------------ SOMA LIBRARY  ------------ <
 Welcome to the Soma Library. Choose one to continue:
 1. View Books 
 2. Login as librarian
 `);
-console.log(welcomeMessage);
 
-let choice = prompt(" Enter your Option:");
+let showLibrary = true;
+while (showLibrary) {
+    console.log(welcomeMessage);
+    let choice = prompt(" Choose your Option:");
+    if (choice === "1") {
+        console.log(`\n------List of Books----\n`)
+        let i = 1;
+        for (const book in listOfBooks) {
+            console.log(i, listOfBooks[book]);
+            i++;
+        }
+    }
+    else if (choice === "2") {
+        const password = "wxyz";
+        for (let tries = 0; tries < 3; tries++) {
+            let input = prompt(" Enter your password: ");
+            if (input === password) {
+                console.log("login successful!");
+                console.log(`> ------------ MENU ------------ <`)
+                for (let i = 0; i < menu.length; i++) {
+                    console.log(`${i + 1} ${menu[i]}`)
+                }
+                let service = prompt(" What service would you like to do? ")
+                if (service === "1") {
+                    console.log(menu[0]);
+                }
+                else if (service === "2") {
+                    console.log(menu[1]);
+                }
+                else if (service === "3") {
 
-if (choice === "1") {
-    console.log(`\n------List of Books----\n`)
-    for (let elem in listOfBooks) {
-        console.log(elem, listOfBooks[elem]);
+                    console.log(menu[2])
+                }
+                else if (service === "4") {
+                    console.log(menu[3]);
+                }
+                break;
+            }
+            else {
+                console.log("log-in unsucessfull please try again!")
+
+            }
+        }
+    }
+    else {
+        console.log("Invalid option ");
     }
 }
-// else if (choice === "2") {
-//     let input = prompt(" Enter your password: "); {
-
-//         let password = "wxyz";
-//         if (input === password) {
-//             console.log("login successful!");
-
-//             console.log(`> ------------ MENU ------------ <`)
-//             for (let i = 0; i < menu.length; i++) {
-//                 console.log(`${i + 1} ${menu[i]}`)
-//             }
-//         }
-//         else {
-//             console.log("log-in unsucessful please try again!")
-//         }
-
-//     }
-
-//     console.log(welcomeMessage);
-
-// }
-
-
-
-
